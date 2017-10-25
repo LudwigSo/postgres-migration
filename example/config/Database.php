696b0064a8113c5063de836c0a27ff358c259011
Ludwig Breitsprecher <ludwig.breitsprecher@tu-ilmenau.de>
2017-10-25 22:23:44 +0200

    Add missing PhpDoc, exclude example directory from classmap

--- example/config/Database.php
+++ example/config/Database.php
@@ -6,43 +6,71 @@ namespace LudwigBr\postgres_migration\example\config;
 use LudwigBr\postgres_migration\config\DatabaseInterface;
 
 
+/**
+ * Class Database
+ * @package LudwigBr\postgres_migration\example\config
+ */
 class Database implements DatabaseInterface
 {
+    /**
+     * @return string
+     */
     public function getDsn() :string
     {
         return $this->getDriver().":host=".$this->getHost().";port=".$this->getPort().";dbname=".$this->getDbName().";user=".$this->getUser().";password=".$this->getPassword();
     }
 
+    /**
+     * @return string
+     */
     public function getDriver() :string
     {
         return 'pgsql';
     }
 
+    /**
+     * @return string
+     */
     public function getHost() :string
     {
         return 'localhost';
     }
 
+    /**
+     * @return int
+     */
     public function getPort() :int
     {
         return 5432;
     }
 
+    /**
+     * @return string
+     */
     public function getDbName() :string
     {
         return 'woh';
     }
 
+    /**
+     * @return string
+     */
     public function getPgDbName() :string
     {
         return 'postgres';
     }
 
+    /**
+     * @return string
+     */
     public function getUser() :string
     {
         return 'postgres';
     }
 
+    /**
+     * @return string
+     */
     public function getPassword() :string
     {
         return 'postgres';
--- example/config/Directories.php
+++ example/config/Directories.php
@@ -6,18 +6,31 @@ namespace LudwigBr\postgres_migration\example\config;
 use LudwigBr\postgres_migration\config\DirectoriesInterface;
 
 
+/**
+ * Class Directories
+ * @package LudwigBr\postgres_migration\example\config
+ */
 class Directories implements DirectoriesInterface
 {
+    /**
+     * @return string
+     */
     public function getPathToSchema() :string
     {
         return __DIR__.'/../result/schema';
     }
 
+    /**
+     * @return string
+     */
     public function getPathToData() :string
     {
         return __DIR__.'/../result/data';
     }
 
+    /**
+     * @return string
+     */
     public function getPathToConstraints() :string
     {
         return __DIR__.'/../result/constraints';
